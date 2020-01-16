72e1a4ed545b07bb0d930c5f49264b3ba27528ff
Alex Creek <me@alexcreek.com>
2020-01-16 00:55:04 -0500

    figure out how to pass headers into a test

--- tests/test_webhook.py
+++ tests/test_webhook.py
@@ -1,5 +1,6 @@
 import json
 import pytest
+from flask import request
 from builder import webhook
 
 @pytest.fixture
@@ -22,11 +23,16 @@ def test_ping(client):
     assert client.get('/ping').status_code == 200
 
 def test_webhook(client, webhook_payload):
- #  This has turned into an integration test.
- #  Disabling for the time being
- #   assert client.post('/webhook', json=webhook_payload).status_code == 200
+#  This has turned into an integration test.
+#  Disabling for the time being
+#   assert client.post('/webhook', json=webhook_payload).status_code == 200
     assert client.post('/webhook').status_code == 400
     assert client.post('/webhook', json={}).status_code == 400
 
 def test_github_ping(client, github_ping_payload):
     assert client.post('/webhook', json=github_ping_payload).status_code == 200
+
+def test_request_headers(client):
+    # https://pytest-flask.readthedocs.io/en/latest/features.html?highlight=headers#request-ctx-request-context
+    res = client.get('/ping', headers=[('X-Something', '42')])
+    assert request.headers['X-Something'] == '42'
